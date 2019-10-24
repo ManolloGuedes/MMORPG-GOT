6029274f11cd8d0eb9d846620a6fa504d98226ed
Herlon Manollo Candido Guedes <manollo.guedes11@gmail.com>
2019-10-24 18:52:38 -0400

    feat(login): login validation

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,3 +1,17 @@
 module.exports.index = (application, req, res) => {
-  res.render('index');
+  res.render('index', {validacao:{}});
+}
+
+module.exports.autenticar = (application, req, res) => {
+  let dadosForm = req.body;
+
+  req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+  req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+  
+  let erros = req.validarionErrors();
+
+  if(erros) {
+    res.render('index', {validacao: erros});
+    return;
+  }
 }
